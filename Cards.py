6e037fe3d9afea3c27b07ff63a0fce89b1a6a18c
MC <68439689+MC-intel@users.noreply.github.com>
2023-05-22 20:37:37 -0500

    Update Cards.py

--- Cards.py
+++ Cards.py
@@ -25,25 +25,10 @@ class CardGame:
 
 import random
 
-class CardGame:
+class BlackjackGame:
     def __init__(self):
-        self.deck = []
         self.player_hand = []
         self.dealer_hand = []
-
-    def create_deck(self):
-        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        
-        self.deck = [(value, suit) for value in values for suit in suits]
-        random.shuffle(self.deck)
-
-    def deal_cards(self):
-        if len(self.deck) < 4:
-            self.create_deck()
-
-        self.player_hand = [self.deck.pop(), self.deck.pop()]
-        self.dealer_hand = [self.deck.pop(), self.deck.pop()]
 
     def calculate_score(self, hand):
         score = 0
@@ -72,5 +57,24 @@ class CardGame:
         player_score = self.calculate_score(self.player_hand)
         dealer_score = self.calculate_score(self.dealer_hand)
 
-        # Rest of the game logic goes here
+        print("Player score:", player_score)
+        print("Dealer score:", dealer_score)
+
+    def create_deck(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        
+        self.deck = [(value, suit) for value in values for suit in suits]
+        random.shuffle(self.deck)
+
+    def deal_cards(self):
+        if len(self.deck) < 4:
+            self.create_deck()
+
+        self.player_hand = [self.deck.pop(), self.deck.pop()]
+        self.dealer_hand = [self.deck.pop(), self.deck.pop()]
+
+# Create an instance of the game and play it
+game = BlackjackGame()
+game.play_game()
 
