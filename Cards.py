5b179cfe8865119618579b99e4c75a8fd5d00a9b
MC <68439689+MC-intel@users.noreply.github.com>
2023-05-22 19:12:10 -0500

    Update Cards.py
    combined methods

--- Cards.py
+++ Cards.py
@@ -19,3 +19,58 @@ class CardGame:
 
         self.player_hand = [self.deck.pop(), self.deck.pop()]
         self.dealer_hand = [self.deck.pop(), self.deck.pop()]
+
+
+#combined methods
+
+import random
+
+class CardGame:
+    def __init__(self):
+        self.deck = []
+        self.player_hand = []
+        self.dealer_hand = []
+
+    def create_deck(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        
+        self.deck = [(value, suit) for value in values for suit in suits]
+        random.shuffle(self.deck)
+
+    def deal_cards(self):
+        if len(self.deck) < 4:
+            self.create_deck()
+
+        self.player_hand = [self.deck.pop(), self.deck.pop()]
+        self.dealer_hand = [self.deck.pop(), self.deck.pop()]
+
+    def calculate_score(self, hand):
+        score = 0
+        has_ace = False
+
+        for card in hand:
+            value = card[0]
+
+            if value.isdigit():
+                score += int(value)
+            elif value in ['J', 'Q', 'K']:
+                score += 10
+            elif value == 'A':
+                has_ace = True
+                score += 1
+
+        if has_ace and score + 10 <= 21:
+            score += 10
+
+        return score
+
+    def play_game(self):
+        self.create_deck()
+        self.deal_cards()
+
+        player_score = self.calculate_score(self.player_hand)
+        dealer_score = self.calculate_score(self.dealer_hand)
+
+        # Rest of the game logic goes here
+
