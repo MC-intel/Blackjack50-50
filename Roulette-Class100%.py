b404bd93f5248709b60cde8c83871ac149a9b4e3
MC <68439689+MC-intel@users.noreply.github.com>
2023-05-26 15:36:53 -0500

    Update Roulette-Class100%.py

--- Roulette-Class100%.py
+++ Roulette-Class100%.py
@@ -1,3 +1,43 @@
+#YOUR COMBINED CLASSES
+
+def __init__(self):
+    self.result = None
+    self.bet_amount = None
+    self.bet_type = None
+
+def spin_wheel(self):
+    self.result = self.random.randint(0, 36)
+    print(f"Roulette wheel has been spun. The pocket number is: {self.result}")
+
+def take_bet(self, amount, bet_type):
+    self.bet_amount = amount
+    self.bet_type = bet_type
+    print(f"Bet of {self.bet_amount} has been placed on {self.bet_type}.")
+
+def play_game(self):
+    if self.bet_amount is None or self.bet_type is None:
+        print("Please place a bet first.")
+    elif self.result is None:
+        print("Please spin the wheel first.")
+    else:
+        if self.bet_type == 'number':
+            if self.check_number_bet():
+                print(f"Congratulations! You've won {self.bet_amount * 36}!")
+            else:
+                print("Sorry, you lost your bet.")
+        elif self.bet_type == 'color':
+            if self.check_color_bet():
+                print(f"Congratulations! You've won {self.bet_amount * 2}!")
+            else:
+                print("Sorry, you lost your bet.")
+
+
+
+
+
+#MY COMBINED CLASSES
+
+
 import random
 
 class Roulette:
